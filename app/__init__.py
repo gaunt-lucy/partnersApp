bd4b5e726e7f11dd97ca55f1c4a64e755e5200c5
Lucy <gaunt.lucy@gmail.com>
2018-07-02 11:46:43 +0100

    create config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config #read the configuration settings
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes #imported at the bottom to resolve circular import issue
