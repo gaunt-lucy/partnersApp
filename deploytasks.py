0d39136b5a602ad7e503424a835b98e6d86fef39
Lucy <gaunt.lucy@gmail.com>
2018-08-25 13:09:09 +0100

    update deployment tasks

--- deploytasks.py
+++ deploytasks.py
@@ -1,7 +1,7 @@
 from flask import app
 from app. models import User, Partner, Mobility, Country, AgreeType, OrgType
 
-User.bulk_add_users()
+#User.bulk_add_users()
 Country.insert_countries()
 Partner.bulk_add_partners()
 AgreeType.add_types()
