92e6a5f74ef15f04d29958fc04bab8e2dcd5bb38
Lucy <gaunt.lucy@gmail.com>
2018-09-02 09:09:38 +0100

    edit deploytasks

--- deploytasks.py
+++ deploytasks.py
@@ -1,11 +1,11 @@
 from flask import app
 from app. models import User, Partner, Mobility, Country, AgreeType, OrgType, AcademicYear
 
-User.bulk_add_users()
-User.adminuser()
+#User.bulk_add_users()
+#User.adminuser()
 Country.insert_countries()
 Partner.bulk_add_partners()
 AgreeType.add_types()
 OrgType.add_types()
-AcademicYear.add_years()
-Mobility.insert_mobs()
+#AcademicYear.add_years()
+#Mobility.insert_mobs()
